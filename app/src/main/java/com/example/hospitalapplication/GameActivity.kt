a5f24899110f91dd86f4ffa9851e6e895ce23d29
ilkhomjon2002 <dergasbaev@gmail.com>
2024-01-22 18:59:58 +0900

    navigation in main page + first Url in game page ware complated

--- app/src/main/java/com/example/hospitalapplication/GameActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/GameActivity.kt
@@ -1,11 +1,27 @@
 package com.example.hospitalapplication
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class GameActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+        val instagram = findViewById<ImageView>(R.id.spotify)
+        instagram.setOnClickListener {
+            val url = "https://poki.com/kr/g/subway-surfers"
+            openUrlInBrowser(url)
+        }
+    }
+
+    private fun openUrlInBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/hospitalapplication/MainActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hospitalapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +11,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
+
+        val navigationProfile = findViewById<LinearLayout>(R.id.navigation_to_ProfileEditAcv)
+        navigationProfile.setOnClickListener{
+            val Intent = Intent(this, ProfileEditActivity::class.java)
+            startActivity(Intent)
+
+        }
+
+        val navigationGame = findViewById<LinearLayout>(R.id.navigation_to_game)
+        navigationGame.setOnClickListener{
+            val Intent = Intent(this, GameActivity::class.java)
+            startActivity(Intent)
+
+        }
+
+        val navigationArt = findViewById<LinearLayout>(R.id.navigation_to_artAct)
+        navigationArt.setOnClickListener{
+            val Intent = Intent(this, ArtActivity::class.java)
+            startActivity(Intent)
+
+        }
+
+        val navigationMedia= findViewById<LinearLayout>(R.id.navigation_to_mediaAct)
+        navigationMedia.setOnClickListener{
+            val Intent = Intent(this, MediaActivity::class.java)
+            startActivity(Intent)
+
+        }
     }
 }
