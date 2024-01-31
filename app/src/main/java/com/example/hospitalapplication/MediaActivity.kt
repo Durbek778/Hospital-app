1c941bddfea56ec5b6ba363aea461e71706fbaf5
ilkhomjon2002 <dergasbaev@gmail.com>
2024-01-31 17:44:14 +0900

    basic logic of media screen is readdy

--- app/src/main/java/com/example/hospitalapplication/MediaActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/MediaActivity.kt
@@ -1,16 +1,29 @@
 package com.example.hospitalapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MediaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
 
+        val bt_video1 = findViewById<Button>(R.id.bt_video1)
+        val bt_video2 = findViewById<Button>(R.id.bt_video2)
+        val bt_video3 = findViewById<Button>(R.id.bt_video3)
+        val bt_video4 = findViewById<Button>(R.id.bt_video4)
 
+        bt_video1.setOnClickListener { playVideo(R.raw.avatar) }
+        bt_video2.setOnClickListener { playVideo(R.raw.iternals) }
+        bt_video3.setOnClickListener { playVideo(R.raw.openhaimer) }
+        bt_video4.setOnClickListener { playVideo(R.raw.video_napaleon) }
+    }
 
+    private fun playVideo(resourceId: Int) {
+        val intent = Intent(this, WatchActivity::class.java)
+        intent.putExtra(WatchActivity.EXTRA_VIDEO_RESOURCE_ID, resourceId)
+        startActivity(intent)
     }
 }
