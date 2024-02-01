0c44992becddf5c74a926f5a6d1ef5621dadbadb
ilkhomjon2002 <dergasbaev@gmail.com>
2024-02-01 14:25:26 +0900

    madia screen development have been complated

--- app/src/main/java/com/example/hospitalapplication/MediaActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/MediaActivity.kt
@@ -2,7 +2,7 @@ package com.example.hospitalapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MediaActivity : AppCompatActivity() {
@@ -10,10 +10,12 @@ class MediaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_media)
 
-        val bt_video1 = findViewById<Button>(R.id.bt_video1)
-        val bt_video2 = findViewById<Button>(R.id.bt_video2)
-        val bt_video3 = findViewById<Button>(R.id.bt_video3)
-        val bt_video4 = findViewById<Button>(R.id.bt_video4)
+        supportActionBar?.hide()
+
+        val bt_video1 = findViewById<LinearLayout>(R.id.bt_video1)
+        val bt_video2 = findViewById<LinearLayout>(R.id.bt_video2)
+        val bt_video3 = findViewById<LinearLayout>(R.id.bt_video3)
+        val bt_video4 = findViewById<LinearLayout>(R.id.bt_video4)
 
         bt_video1.setOnClickListener { playVideo(R.raw.avatar) }
         bt_video2.setOnClickListener { playVideo(R.raw.iternals) }
