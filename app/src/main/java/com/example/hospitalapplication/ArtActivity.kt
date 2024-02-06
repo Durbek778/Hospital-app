bff507da3bf5a6a9d958f06ebb7ae766364b12ec
ilkhomjon2002 <dergasbaev@gmail.com>
2024-02-06 16:54:23 +0900

    art datailed is nearly readdy

--- app/src/main/java/com/example/hospitalapplication/ArtActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/ArtActivity.kt
@@ -1,11 +1,22 @@
 package com.example.hospitalapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class ArtActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_art)
+
+        supportActionBar?.hide()
+
+        val navigationGame = findViewById<LinearLayout>(R.id.navigation_to_Art_Datailed)
+        navigationGame.setOnClickListener {
+            val Intent = Intent(this, ArtDetailedActivity::class.java)
+            startActivity(Intent)
+
+        }
     }
 }
