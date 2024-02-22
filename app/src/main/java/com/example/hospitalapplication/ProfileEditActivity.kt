468042f10eab9606237809880c902974563efda7
ilkhomjon2002 <dergasbaev@gmail.com>
2024-02-22 22:13:07 +0900

    supabase is connection to mainscreen is complated

--- app/src/main/java/com/example/hospitalapplication/ProfileEditActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/ProfileEditActivity.kt
@@ -1,26 +1,26 @@
-package com.example.hospitalapplication
-
-import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.launch
-
-
-class ProfileEditActivity : AppCompatActivity() {
-    lateinit var patient_name: TextView
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile_edit)
-
-        supportActionBar?.hide()
-
-
-        lifecycleScope.launch(handler) {
-            supabase = Requests()
-
-        }
-    }
-}
+//package com.example.hospitalapplication
+//
+//import android.os.Bundle
+//import android.widget.TextView
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.lifecycle.lifecycleScope
+//import kotlinx.coroutines.launch
+//
+//
+//class ProfileEditActivity : AppCompatActivity() {
+//    lateinit var patient_name: TextView
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_profile_edit)
+//
+//        supportActionBar?.hide()
+//
+//
+//        lifecycleScope.launch(handler) {
+//            supabase = Requests()
+//
+//        }
+//    }
+//}
