e0667d088a00c6166d8753f89a700e3c6ca6d1f9
Ilkhomjon2002 <ilhommaxsudov3@gmail.com>
2024-02-22 18:20:03 +0900

    Supabase init

--- app/src/main/java/com/example/hospitalapplication/GameActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/GameActivity.kt
@@ -5,13 +5,17 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class GameActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        lifecycleScope.launch {
+            supabase.getGames()
+        }
 
         supportActionBar?.hide()
 
--- app/src/main/java/com/example/hospitalapplication/MainActivity.kt
+++ app/src/main/java/com/example/hospitalapplication/MainActivity.kt
@@ -3,12 +3,45 @@ package com.example.hospitalapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
+
+lateinit var supabase: Requests
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var pulse_text: TextView
+    lateinit var pills_text: TextView
+    lateinit var infusion_text: TextView
+    suspend fun initSupabase() {
+        supabase = Requests()
+    }
+
+    fun updatePatientInfo() {
+        pulse_text.text = supabase.patients.heartRate.toString() + " BPM";
+        infusion_text.text = supabase.patients.bloodGlucose.toString() + " BPM";
+        pills_text.text = supabase.patients.bloodPressure.toString() + " BPM";
+    }
+
+    fun initUI() {
+        pulse_text = findViewById(R.id.pulse_text)
+        infusion_text = findViewById(R.id.infusion_text)
+        pills_text = findViewById(R.id.pills_text)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
+
+        lifecycleScope.launch(handler) {
+            initSupabase()
+            supabase.getPatientInfo()
+            updatePatientInfo()
+
+        }
 
         supportActionBar?.hide()
 
@@ -40,4 +73,6 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+
 }
